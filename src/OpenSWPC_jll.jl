32988e5507ba411573eecd765aa0ccc2583be932
Boris Kaus <boriskaus@gmail.com>
2026-09-23 20:58:13 +0200

    OpenSWPC_jll build 26.9.1+0

--- src/OpenSWPC_jll.jl
+++ src/OpenSWPC_jll.jl
@@ -2,11 +2,10 @@
 baremodule OpenSWPC_jll
 using Base
 using Base: UUID
-using LazyArtifacts
 using MPIPreferences
 Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("OpenSWPC")
-JLLWrappers.@generate_main_file("OpenSWPC", UUID("97e963f0-2dee-5a96-a601-8cee97e19b5a"))
+JLLWrappers.@generate_main_file("OpenSWPC", Base.UUID("97e963f0-2dee-5a96-a601-8cee97e19b5a"))
 end  # module OpenSWPC_jll
